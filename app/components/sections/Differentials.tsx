1ad2fa273ace96a0b71e4b1588f0cd3753cd2308
Pedro Hammes <predohammes@gmail.com>
2026-04-02 14:19:50 -0300

    feat: Differentials com texto sobre imagem, degradê e bordas arredondadas

--- app/components/sections/Differentials.tsx
+++ app/components/sections/Differentials.tsx
@@ -5,10 +5,12 @@ export default function Differentials() {
     
     const details = [
         {title: "Claraboia", description: "   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam suscipit deleniti vero natus voluptates beatae alias maxime impedit officiis dolores.", image: "/images/20260316_105845.jpg"},
-        {title: "Cozinh", description: "   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam suscipit deleniti vero natus voluptates beatae alias maxime impedit officiis dolores.", image: "/images/20260316_105845.jpg"},
-        {title: "Quarto", description: "   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam suscipit deleniti vero natus voluptates beatae alias maxime impedit officiis dolores.", image: "/images/20260316_105845.jpg"}
+        {title: "Pitfire", description: "   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam suscipit deleniti vero natus voluptates beatae alias maxime impedit officiis dolores.", image: "/images/20260316_105845.jpg"},
+        {title: "Ofurô", description: "   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam suscipit deleniti vero natus voluptates beatae alias maxime impedit officiis dolores.", image: "/images/20260316_105845.jpg"},
+        {title: "Rede Suspensa", description: "   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam suscipit deleniti vero natus voluptates beatae alias maxime impedit officiis dolores.", image: "/images/20260316_105845.jpg"}
     ]
   return <section id="differentials" className="px-6 py-16 md:px-12">
+    <h2>A Cabana - diferenciais</h2>
     <Carousel>
         <CarouselContent>
             {
@@ -18,18 +20,22 @@ export default function Differentials() {
                             basis-1/2 lg:basis-1/3
                             "
                             >
-                                <p>{detail.title}</p>
-                                <p>{detail.description}</p>
-                            <div className="
-                            relative h-80
-                            ">
-                                {<Image
-                                    src={detail.image}
-                                    fill
-                                    alt={detail.title}
-                                    className="object-cover"
-                                />}
-                            </div>
+                                <div className="
+                                relative h-80
+                                rounded-2xl overflow-hidden
+                                ">
+                                    {<Image
+                                        src={detail.image}
+                                        fill
+                                        alt={detail.title}
+                                        className="object-cover"
+                                    />}
+                                    <div className="absolute inset-0 z-5 bg-linear-to-t from-black/70 to-transparent" />
+                                    <div className="absolute bottom-0 left-0 right-0 z-10 p-4">
+                                        <p>{detail.title}</p>
+                                        <p>{detail.description}</p>
+                                    </div>
+                                </div>
                             
 
                         </CarouselItem>
