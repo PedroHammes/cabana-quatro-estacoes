f799ee8a5dd501025e1b4ca44b3690491aa946c2
Pedro Hammes <predohammes@gmail.com>
2026-04-02 11:49:14 -0300

    style: tamanhos globais para headings e layout About corrigido

--- app/components/sections/About.tsx
+++ app/components/sections/About.tsx
@@ -3,16 +3,11 @@ import Image from "next/image";
 export default function About() {
   return <section id="about" className="px-6 py-16 md:px-12">
     <div>
-        <h1>SOBRE</h1>
+        <h1>A Cabana</h1>
     </div>
     <div className="
     flex flex-col md:flex-row
     ">
-        <div className="
-        flex-1
-        ">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa expedita quaerat eaque? Eligendi nihil, fugit earum dolore at quae! Ducimus maiores consectetur reprehenderit delectus! Ipsa dolorem totam sunt numquam molestias dolorum iure sint quo tempora eligendi! Impedit sapiente vero ratione, sint voluptatibus qui doloribus, laboriosam ipsum eius facilis recusandae! Exercitationem recusandae ipsum molestias quia eos corporis dignissimos odio voluptas consectetur.</p>
-        </div>
         <div className="
         relative
         h-100 md:w-1/2
@@ -27,6 +22,17 @@ export default function About() {
                 "
             />
         </div>
+        <div className="
+        flex-1
+        ">
+            <h2>A Cabana</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa expedita quaerat eaque? Eligendi nihil, fugit earum dolore at quae! Ducimus maiores consectetur reprehenderit delectus! </p>
+
+            <p>Ipsa dolorem totam sunt numquam molestias dolorum iure sint quo tempora eligendi! Impedit sapiente vero ratione, sint voluptatibus qui doloribus, laboriosam ipsum eius facilis recusandae! </p>
+
+            <p className="font-bold">Exercitationem recusandae ipsum molestias quia eos corporis dignissimos odio voluptas consectetur.</p>
+        </div>
+
     </div>
   </section>
 }
