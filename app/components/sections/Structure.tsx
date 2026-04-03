897127c21430a14f22d93cb2709bad44b176ec98
Pedro Hammes <predohammes@gmail.com>
2026-04-03 12:32:03 -0300

    feat: Structure com carrossel ativo/inativo, altura e largura corretas

--- app/components/sections/Structure.tsx
+++ app/components/sections/Structure.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import {
   Carousel,
@@ -8,6 +9,8 @@ import {
 } from "@/components/ui/carousel"
 
 import Image from "next/image";
+import { type CarouselApi } from "@/components/ui/carousel"
+import React from "react";
 
 export default function Structure() {
     const rooms = [
@@ -18,46 +21,76 @@ export default function Structure() {
         { title: "Quarto", description: "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate voluptatem ullam voluptas esse natus quam fugit aliquam neque, explicabo distinctio?", image: "https://lh3.googleusercontent.com/p/AF1QipO8kfGvD5_52vBDjP31lEpaFGxjxO_jyOB-JIix=w154-h258-n-k-no-nu"
         }
     ]
-  return <section id="structure" className="px-6 py-16 md:px-12 snap-start min-h-screen flex flex-col items-center justify-center">
-    <h2>A Cabana - estrutura</h2>
-    <Carousel className="w-full">
-        <CarouselContent>
-            {
-                rooms.map( (room) => {
-                        return <CarouselItem key={room.title}
-                            className="
-                            basis-1/2 lg:basis-1/3
-                            "
-                            >
-                            <div className="
-                            relative h-80
-                            rounded-2xl overflow-hidden
-                            ">
-                                {<Image
-                                    src={room.image}
-                                    fill
-                                    alt={room.title}
-                                    className="object-cover"
-                                />}
-                                <div className="absolute inset-0 z-5 bg-linear-to-t from-black/70 to-transparent" /> 
-                                <div className="absolute bottom-0 left-0 right-0 z-10 p-4">
-                                    <p>{room.title}</p>
-                                    <p>{room.description}</p>
-                                </div>
-                            </div>
-                            
 
-                        </CarouselItem>
-                })
-            }
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-    </Carousel>
-    <div className="flex justify-center mt-4">
+    const [api, setApi] = React.useState<CarouselApi>()
+    const [current, setCurrent] = React.useState(0)
+
+    React.useEffect(() => {
+        if (!api) return
+    api.on("select", () => {
+        setCurrent(api.selectedScrollSnap())
+    })
+    }, [api])
+
+    
+  return <section id="structure"
+  className="
+  snap-start h-screen
+  py-16 md:py-32 mx-4
+  flex flex-col flex-1 items-center justify-center
+  ">
+
+    {/*Card principal*/}
+    <div className="
+    flex flex-col flex-1 h-full md:flex-row md:min-h-80
+    rounded-2xl overflow-visible
+    bg-olive-500
+    ">
+        <Carousel setApi={setApi} className="w-full h-full flex-1 min-h-0 [&>div]:h-full">
+            <CarouselContent className="-ml-4 h-full">
+                {
+                    rooms.map( (room, index) => {
+                        const isActive = index == current
+                            return (
+                            <CarouselItem key={room.title}
+                            className={isActive 
+                                    ? "basis-[70%] h-full transition-all duration-500"
+                                    : "basis-[30%] h-full transition-all duration-500"}
+                                >
+                                <div className="
+                                relative h-full rounded-2xl overflow-hidden
+                                flex flex-col
+                                ">
+                                    <Image
+                                        src={room.image}
+                                        fill
+                                        alt={room.title}
+                                        className="object-cover"
+                                    />
+                                    <div className="absolute inset-0 z-5 bg-linear-to-t from-black/70 to-transparent"></div> 
+                                    <div className="
+                                    relative mt-auto p-4 z-10
+                                    
+                                    ">
+                                        <h3>{room.title}</h3>
+                                        {isActive && <p>{room.description}</p>}
+                                    </div>
+                                </div>
+                            </CarouselItem>
+                            )
+                    })
+                }
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    </div>
+
+
+    {/* <div className="flex justify-center mt-4">
         <Button>
             Veja todas as fotos
         </Button>
-    </div>
+    </div> */}
   </section>
 }
