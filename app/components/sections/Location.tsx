2928af9309e8428e4cf9d1fef37a31db4f3350ca
Pedro Hammes <predohammes@gmail.com>
2026-04-13 01:16:59 -0300

    fix: correção de location

--- app/components/sections/Location.tsx
+++ app/components/sections/Location.tsx
@@ -4,43 +4,70 @@ import Image from "next/image";
 export default function Location(Props: {setOpen: (value: boolean) => void}) {
   return <section id="location"
   className="
-    h-screen
+    min-h-screen
     py-16 md:py-32 mx-4 lg:mx-32
     flex flex-col items-center justify-center
+    bg-red-500
   ">
     {/* Card principal */}
     <div className="
-    flex flex-col flex-1 h-full w-full md:flex-row md:min-h-80 gap-4
-    rounded-2xl overflow-hidden 
-    bg-olive-500
+    flex flex-col md:flex-row-reverse flex-1 h-full w-full md:min-h-80 gap-4
+    rounded-2xl
+    bg-[#a9a685]
     ">
-      {/* Card da esquerda */}
-      <div className="w-full md:w-4/10 h-full">
+      {/* Card de cima */}
+      <div className="
+      relative rounded-2xl w-full md:w-6/10 h-full text-white
+      flex flex-col
+      p-4 lg:p-8
+      gap-4 lg:gap-8
+      text-base lg:text-lg
+      ">
+        <div>
+          <h4>Como chegar</h4>
+          <p>
+              Localizada no Vale do Café no Estado do Rio de Janeiro, a 112 km do Rio de Janeiro, e a 4 km do Vale das Videiras, em Araras, Petrópolis.
+          </p>
+        </div>
+        <img src="/ux/06-location/location.png" alt="Como chegar" className="rounded-2xl"/>
+        <div className="flex flex-col gap-4">
+          <h4>Atenção:</h4>
+          <p className="border-2 rounded-lg border-olive-50 p-2">
+            Para quem vem pelo Vale das Videiras, Araras ou Petrópolis:
+            Siga sempre pela RJ-117 até o Posto de Saúde de Maravilha / Paty do Alferes.
+          </p>
+          <p className="border-2 rounded-lg border-olive-50 p-2">
+            Insira o endereço no GPS antes de entrar na estrada de terra, pois o sinal pode oscilar na região rural.
+            Link do Google Maps: https://maps.app.goo.gl/6bszPtjdBuShpk4o8
+          </p>
+        </div>
+
+      </div>
+
+      {/* Card de baixo */}
+      <div className="
+      w-full md:w-4/10 h-full
+      flex flex-col
+      p-4 lg:p-8
+      gap-4 lg:gap-8
+      text-base lg:text-lg
+      ">
+      <p className="p-2">
+        Endereço: Condomínio Fazenda Boa Vista – Estrada Sítio Barreiros, nº 1.801, Casa 3 – Fazenda Boa Vista, Bairro Maravilha – Paty do Alferes – CEP 26.950-000.
+
+      </p>
         <iframe 
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d471395.6141939803!2d-43.75603247557016!3d-22.62475499404772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x991b347265164b%3A0x3c26c429e547b567!2sCabana%20Quatro%20Esta%C3%A7%C3%B5es%20-%20Seu%20ref%C3%BAgio%20rom%C3%A2ntico%20na%20Serra%20do%20RJ!5e0!3m2!1spt-BR!2sbr!4v1774124570089!5m2!1spt-BR!2sbr" 
         
         className="
-        w-full h-full border-0 rounded-2xl
+        w-full border-0 rounded-2xl
+        h-100 lg:h-166
         "
         allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade">
         </iframe>
       </div>
 
-      {/* Card da direita */}
-      <div className="relative rounded-2xl overflow-hidden w-full md:w-6/10 h-full text-white">
-        <Image
-          src="/ux/06-location/location.png"
-          fill
-          alt="Como chegar"
-          className="object-cover"
-        />
-        <div className="absolute inset-0 z-5 bg-linear-to-b from-transparent to-black/70"></div>
-        <div className="absolute inset-0 z-10 flex items-center p-8">
-          <p>
-            Localizada no Vale do Café no Estado do Rio de Janeiro, a 112 km do Rio de Janeiro, e a 4 km do Vale das Videiras, em Araras, Petrópolis.
-          </p>
-        </div>
-      </div>
+
 
     </div>
 
