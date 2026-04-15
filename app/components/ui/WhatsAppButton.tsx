24c80ba543fed094550e075457e68747a84844c2
Pedro Hammes <predohammes@gmail.com>
2026-04-14 21:15:28 -0300

    fix(hero): centralizar titulo e botão, remover logo mobile e att número do whatsapp

--- app/components/ui/WhatsAppButton.tsx
+++ app/components/ui/WhatsAppButton.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 export default function WhatsAppButton(Props: {setOpen: (value: boolean) => void}) {
   return <Button id="whatsappbutton" 
   className="
-  fixed bottom-4 right-4
+  fixed bottom-4 left-1/2 -translate-x-1/2 z-10
   rounded-full  hover:bg-[#4a5c44] hover:color-[#FFFFFF]  cursor-pointer"
   style={{ color: '#ffffff', fontWeight: 'bold' }}
   onClick={ () => Props.setOpen(true) }
