4d0f260eaeea87268955f98dc6f7f58ea388ea6c
Pedro Hammes <predohammes@gmail.com>
2026-04-19 18:22:18 -0300

    fix(team): ajuste de proporções e layout

--- app/components/sections/Team.tsx
+++ app/components/sections/Team.tsx
@@ -13,16 +13,16 @@ export default function Team(Props: {setOpen: (value: boolean) => void}) {
         }
     ]
 
-  return <section id="team" className="snap-start ">
+  return <section id="team" className="snap-start">
     <div 
     className="
-    mx-0 lg:mx-32 px-4 max-w-full mb-4
+    mx-0 lg:mx-64 px-4 max-w-full mb-4
     flex flex-col flex-1 items-center justify-center
     ">
         <div className="
-            flex flex-col md:flex-row flex-1 h-full w-full md:min-h-80
-            rounded-2xl overflow-hidden
-            bg-[#a9a685] text-white
+        flex flex-col md:flex-row flex-1 h-full w-full md:min-h-80 lg:min-h-144
+        rounded-2xl overflow-hidden
+        bg-[#a9a685] text-white
         ">
             {team.map((member) => (
                 <div key={member.id} className="flex flex-col md:flex-row w-full">
@@ -36,15 +36,20 @@ export default function Team(Props: {setOpen: (value: boolean) => void}) {
                         />
                     </div>
                     {/* Texto */}
-                    <div className="flex-1 p-4 flex flex-col justify-center gap-4">
-                        <h3>{member.name}</h3>
-                        <p className="text-sm tracking-widest">{member.title}</p>
-                        <p>{member.description}</p>
+                    <div className="flex-1 p-4 lg:p-12 flex flex-col justify-center gap-4 lg:gap-8">
+                        <h3>
+                            {member.name}
+                            <br />
+                            <span className="text-sm lg:text-xl font-thin tracking-widest">
+                                {member.title}
+                            </span>
+                        </h3>
+                        <p className="text-sm lg:text-xl">{member.description}</p>
                         <a 
                             href="https://wa.me/5521999439858" 
                             target="_blank" 
                             rel="noopener noreferrer"
-                            className="mt-4 border border-white rounded-full px-6 py-2 w-fit"
+                            className="mt-4 lg:mt-32 border rounded-full px-6 py-2 w-fit bg-olive-500 text-white"
                             >
                             Fale com {member.name.split(" ")[0]}
                         </a>
