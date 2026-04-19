f0f36e889856b87b1559139aba61399837421e6a
Pedro Hammes <predohammes@gmail.com>
2026-04-19 16:52:22 -0300

    fix(location): ajuste das divs

--- app/components/sections/Location.tsx
+++ app/components/sections/Location.tsx
@@ -10,16 +10,16 @@ export default function Location(Props: {setOpen: (value: boolean) => void}) {
     <h2>Localização</h2>
     {/* Card principal */}
     <div className="
-    flex flex-col md:flex-row-reverse flex-1 h-full w-full md:min-h-80 gap-2
+    flex flex-col md:flex-row-reverse flex-1 w-full md:min-h-80 lg:h-160 gap-2
     rounded-2xl
     bg-[#a9a685]
     ">
       {/* Card de cima */}
       <div className="
-      relative rounded-2xl w-full md:w-6/10 h-full text-white
+      relative rounded-2xl w-full md:w-6/10 h-full text-white bg-[#8a8768]
       flex flex-col
-      p-4 lg:p-8
-      gap-4 lg:gap-8
+      p-4 lg:p-12
+      gap-4
       text-base lg:text-lg
       ">
         <div>
@@ -28,7 +28,7 @@ export default function Location(Props: {setOpen: (value: boolean) => void}) {
               Localizada no Vale do Café no Estado do Rio de Janeiro, a 112 km do Rio de Janeiro, e a 4 km do Vale das Videiras, em Araras, Petrópolis.
           </p>
         </div>
-        <img src="/ux/06-location/location.png" alt="Como chegar" className="rounded-2xl"/>
+        <img src="/ux/06-location/location.png" alt="Como chegar" className="rounded-2xl lg:h-64 lg:object-cover"/>
         <div className="flex flex-col gap-4">
           <h4>Atenção:</h4>
           <p className="border-2 rounded-lg border-olive-50 p-2">
@@ -40,11 +40,24 @@ export default function Location(Props: {setOpen: (value: boolean) => void}) {
             Link do Google Maps: <a href="https://maps.app.goo.gl/6bszPtjdBuShpk4o8" target="_blank" rel="noopener noreferrer" className="text-blue-700">Cabana Quatro Estações - Seu regúgio romântico na Serra do RJ</a>
           </p>
         </div>
+        <Button
+          onClick={() => Props.setOpen(true)}
+          className="
+          rounded-full 
+          bg-[#4e6646]
+          text-lg
+          cursor-pointer
+          lg:w-1/2
+          self-center
+          "
+          >
+          Entre em contato e tire suas dúvidas
+        </Button>
       </div>
 
       {/* Card de baixo */}
       <div className="
-      w-full md:w-4/10 h-full
+      w-full md:w-4/10 lg:flex-1
       flex flex-col
       p-4 lg:p-8
       gap-4 lg:gap-8
@@ -58,8 +71,7 @@ export default function Location(Props: {setOpen: (value: boolean) => void}) {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d471395.6141939803!2d-43.75603247557016!3d-22.62475499404772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x991b347265164b%3A0x3c26c429e547b567!2sCabana%20Quatro%20Esta%C3%A7%C3%B5es%20-%20Seu%20ref%C3%BAgio%20rom%C3%A2ntico%20na%20Serra%20do%20RJ!5e0!3m2!1spt-BR!2sbr!4v1774124570089!5m2!1spt-BR!2sbr" 
         
         className="
-        w-full border-0 rounded-2xl
-        h-100 lg:h-166
+        w-full border-0 rounded-2xl h-100 lg:flex-1
         "
         allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade">
         </iframe>
